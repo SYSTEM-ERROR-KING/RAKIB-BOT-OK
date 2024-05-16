4f41cb1307e6ba905213a2773742ef867c0fbcca
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-05-16 12:30:07 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -48,7 +48,7 @@ module.exports.run = async function({ api, event, Users }) {
   if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
 
-    let gifUrl = 'https://i.postimg.cc/1tSMPx2y/R.jpg';
+    let gifUrl = 'https://i.imgur.com/pa3G2ZN.mp4';
 let gifPath = __dirname + '/Nayan/join/R.jpg';
 
 axios.get(gifUrl, { responseType: 'arraybuffer' })
@@ -104,11 +104,11 @@ https://www.facebook.com/SYSTEM.ERROR.KING
         let avtAnime = (await axios.get(encodeURI(
           `https://graph.facebook.com/${event.logMessageData.addedParticipants[o].userFbId}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`), { responseType: "arraybuffer" })).data;
         var ok = [
-          'https://i.imgur.com/dDSh0wc.jpeg',
-          'https://i.imgur.com/UucSRWJ.jpeg',
-          'https://i.imgur.com/OYzHKNE.jpeg',
-          'https://i.imgur.com/V5L9dPi.jpeg',
-          'https://i.imgur.com/M7HEAMA.jpeg'
+          'https://i.imgur.com/pa3G2ZN.mp4',
+          'https://i.imgur.com/pa3G2ZN.mp4',
+          'https://i.imgur.com/pa3G2ZN.mp4',
+          'https://i.imgur.com/pa3G2ZN.mp4',
+          'https://i.imgur.com/pa3G2ZN.mp4'
         ]
         let background = (await axios.get(encodeURI(`${ok[Math.floor(Math.random() * ok.length)]}`), { responseType: "arraybuffer", })).data;
         fs.writeFileSync(pathAva, Buffer.from(avtAnime, "utf-8"));
